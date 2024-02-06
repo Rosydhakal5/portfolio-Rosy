b157d47cef860dac1ff84dfe6c95b115da5a22f9
Rosy <rosydhakal333@gmail.com>
2024-02-06 13:17:01 +1100

    creating components

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './components/Home'
+import About from './components/About'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import Qualification from './components/Qualification'
+import Experience from './components/Experience'
+import Navbar from './navcomponents/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <p> This is my portfolio</p>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Qualification />
+      <Experience />
+
+
+
     </>
   )
 }
